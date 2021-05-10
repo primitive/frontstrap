7ee7dc29b5a6a3ec39853a6f0bb699d2af732236
Shaun Knowles <shaun@primitivedigital.uk>
2021-05-10 08:27:17 +0100

    add custom theme + source config

--- frontity.settings.js
+++ frontity.settings.js
@@ -30,10 +30,15 @@ const settings = {
             ["Primitive Theme for Frontity", "https://www.npmjs.com/package/primitive-theme"],
             ["Privacy Policy", "privacy-policy"]
           ],
-          
+
           "featured": {
             "showOnList": false,
-            "showOnPost": false
+            "showOnPost": false,
+            "showOnPage": true
+          },
+          // tbc
+          "archive": {
+            "showExcerpt": true
           }
         }
       }
@@ -42,12 +47,48 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://api.primitivedigital.uk"
+          "url": "https://api.primitivedigital.uk",
+
+          "homepage": "/home", // optional: set if using a page as homepage (wp hp settings ignored)
+          "postsPage": "/blog", // optional: set if using a custom blog page
+
+          // optional: CPTs
+          "postTypes": [
+            {
+              "type": "works", // slug
+              "endpoint": "works", // WP-REST endpoint
+              "archive": "/works" // router link: do i need this if not diplying the list!?
+            },
+            {
+              "type": "temporal_events",
+              "endpoint": "temporal_events",
+              "archive": "/evolution-of-digital-stuff" // list view of the CPT
+            },
+            {
+              "type": "things",
+              "endpoint": "things",
+              "archive": "/things"
+            }
+          ],
+
+          "taxonomies": [
+            {
+              "taxonomy": "timelines",
+              "endpoint": "timelines",
+              "postTypeEndpoint": "/temporal_events",
+            }
+          ]
+
         }
       }
     },
     "@frontity/tiny-router",
-    "@frontity/html2react"
+    "@frontity/html2react",
+    "@frontity/yoast",
+    //"frontity-contact-form-7",
+    //"primitiverocks",
+    //"primitivepebbles",
+    //"primitivescenes"
   ]
 };
 
