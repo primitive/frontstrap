97f10b71bfe979698cd1a1b1abb952998da84624
Shaun Knowles <shaun@primitivedigital.uk>
2022-01-23 14:00:40 +0000

    wrap app with SSRProvider

--- packages/frontstrap/src/components/index.js
+++ packages/frontstrap/src/components/index.js
@@ -1,6 +1,9 @@
 import { connect, Global, Head, styled, loadable } from "frontity";
 import Switch from "@frontity/components/switch";
 
+// see: https://react-bootstrap.github.io/getting-started/server-side-rendering/
+import SSRProvider from 'react-bootstrap/SSRProvider';
+
 import Loading from "@primitive/pebbles/loading";
 import MetaTitle from "@primitive/pebbles/meta-title";
 import globalStyles from "./dust/global-styles";
@@ -51,74 +54,76 @@ const Theme = ({ state }) => {
   }
 
   return (
-    <App>
+    <SSRProvider>
+      <App>
 
-      <Global styles={globalStyles(state.theme.config, state.theme.colors)} /> 
-      {/* reboot and styled html elements */}
+        <Global styles={globalStyles(state.theme.config, state.theme.colors)} /> 
+        {/* reboot and styled html elements */}
 
-      { /* <FontFaces /> */}
+        { /* <FontFaces /> */}
 
-      <MetaTitle />
-      {/* Adds metatags */}
+        <MetaTitle />
+        {/* Adds metatags */}
 
-      <Head>
-        
-        <meta name="description" content={state.frontity.description} />
-        <html lang="en" />
+        <Head>
+          
+          <meta name="description" content={state.frontity.description} />
+          <html lang="en" />
 
-        <link rel="canonical" href={state.theme.canonical} />
+          <link rel="canonical" href={state.theme.canonical} />
 
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Amatic+SC|Hepta+Slab|Pacifico|Playfair+Display:700|Slabo+27px&display=swap" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Amatic+SC|Hepta+Slab|Pacifico|Playfair+Display:700|Slabo+27px&display=swap" />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous" />
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-alpha2/css/bootstrap-grid.min.css" integrity="sha512-Jn+RkWIYxM5Cn3mfAWyV7CgxFnDFxe3EBh93974boKdhcAUE9TSr7qJTJNlzt+J2wG6a3sLYnEyRF+1/o01u9Q==" crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-alpha2/css/bootstrap-utilities.min.css" integrity="sha512-mkkoFJEfBE2cFvIICxddh2VS0uB//H6/hf7siESrN2H7NiyzoGwZ5Rk3Ta12mPxlNOVq5IjhQ97YBK11vldYug==" crossorigin="anonymous" />
-        
-        <link rel="preconnect" SameSite="None" Secure href="https://www.google-analytics.com" />
-        { /* <link rel="dns-prefetch" SameSite="None" Secure href="https://www.google-analytics.com" /> */ }
-        <link rel="dns-prefetch" SameSite="None" Secure href="http://doubleclick.net" />
-        
-      </Head>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-alpha2/css/bootstrap-grid.min.css" integrity="sha512-Jn+RkWIYxM5Cn3mfAWyV7CgxFnDFxe3EBh93974boKdhcAUE9TSr7qJTJNlzt+J2wG6a3sLYnEyRF+1/o01u9Q==" crossorigin="anonymous" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-alpha2/css/bootstrap-utilities.min.css" integrity="sha512-mkkoFJEfBE2cFvIICxddh2VS0uB//H6/hf7siESrN2H7NiyzoGwZ5Rk3Ta12mPxlNOVq5IjhQ97YBK11vldYug==" crossorigin="anonymous" />
+          
+          <link rel="preconnect" SameSite="None" Secure href="https://www.google-analytics.com" />
+          { /* <link rel="dns-prefetch" SameSite="None" Secure href="https://www.google-analytics.com" /> */ }
+          <link rel="dns-prefetch" SameSite="None" Secure href="http://doubleclick.net" />
+          
+        </Head>
 
-      {/* Accessibility: skip to main content */}
-      <SkipLink as="a" href="#main">Skip to main content</SkipLink>
+        {/* Accessibility: skip to main content */}
+        <SkipLink as="a" href="#main">Skip to main content</SkipLink>
 
-      { /* Load global Nav/Header */}
-      <Header />
+        { /* Load global Nav/Header */}
+        <Header />
 
-      {/* Render content for the post(s), page, cpt(s) here */}
-      <Main
-        id="main"
-        bg={display.main.bg}
-        bordertop={display.main.bordertop}
-        borderbottom={display.main.borderbottom}
-      >
+        {/* Render content for the post(s), page, cpt(s) here */}
+        <Main
+          id="main"
+          bg={display.main.bg}
+          bordertop={display.main.bordertop}
+          borderbottom={display.main.borderbottom}
+        >
 
-        <Switch>
-          <Loading when={data.isFetching} />
-          <Home when={data.isHome} />
-          <Page when={data.isPage} />
-          <Posts when={data.isPostArchive} />
-          <Post when={data.isPost} />
-          <Timeline when={data.isTemporalEventsArchive} />
-          <SubTimeline when={data.isTimelineType} />
-          <Timelines when={data.isTimelines} />
-          <PageError when={data.isError} />
-        </Switch>
+          <Switch>
+            <Loading when={data.isFetching} />
+            <Home when={data.isHome} />
+            <Page when={data.isPage} />
+            <Posts when={data.isPostArchive} />
+            <Post when={data.isPost} />
+            <Timeline when={data.isTemporalEventsArchive} />
+            <SubTimeline when={data.isTimelineType} />
+            <Timelines when={data.isTimelines} />
+            <PageError when={data.isError} />
+          </Switch>
 
-      </Main>
+        </Main>
 
-      {display.showPromo && <GlobalPromo />} { /* Promo section: (optional) */}
+        {display.showPromo && <GlobalPromo />} { /* Promo section: (optional) */}
 
-      {display.showSocial && <GlobalSocial />} { /* Social section: (optional) */}
+        {display.showSocial && <GlobalSocial />} { /* Social section: (optional) */}
 
-      <KnobblyKnees /> { /* CTA section: (optional) */}
+        <KnobblyKnees /> { /* CTA section: (optional) */}
 
-      <FurryBoots /> { /* Assurance section: brands/reviws/offers (optional) */}
+        <FurryBoots /> { /* Assurance section: brands/reviws/offers (optional) */}
 
-      <Feet /> { /* footer */}
+        <Feet /> { /* footer */}
 
-    </App>
+      </App>
+    </SSRProvider>
   );
 };
 
